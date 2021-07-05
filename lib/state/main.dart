7167693a537b659acf1940d076db15e7285157fd
Robotoaster <swav.kulinski@gmail.com>
2021-07-05 15:57:22 +0100

    simple state

--- lib/state/main.dart
+++ lib/state/main.dart
@@ -26,21 +26,43 @@ class MyHomePage extends ConsumerWidget {
   }) : super(key: key);
   @override
   Widget build(context, ref) {
-    final state = ref(stateProvider);
+    final addA = ref(addAProvider);
+    final addB = ref(addBProvider);
+    final removeA = ref(removeAProvider);
+    final removeB = ref(removeBProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lab'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _Text(),
-          ],
+        appBar: AppBar(
+          title: Text('Lab'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () => state.state = AppState(a:state.state.a + 'a', b:state.state.b),),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _Text(),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: FloatingActionButton(child: Text('+a'), onPressed: addA),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: FloatingActionButton(child: Text('+b'), onPressed: addB),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: FloatingActionButton(child: Text('-a'), onPressed: removeA),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: FloatingActionButton(child: Text('-b'), onPressed: removeB),
+            ),
+          ],
+        ));
   }
 }
 
@@ -48,8 +70,21 @@ class _Text extends ConsumerWidget {
   @override
   Widget build(context, ref) {
     final a = ref(stateProvider);
-    return Text(
-      'hashCode ${a.hashCode} ${a.state.a}',
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'provider hashCode ${a.hashCode}',
+        ),
+        Divider(),
+        Text(
+          'a.a ${a.state.a}',
+        ),
+        Divider(),
+        Text(
+          '${a.hashCode}',
+        ),
+      ],
     );
   }
 }
--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,12 +1,24 @@
 class AppState {
-  
   String _a;
   String get a => _a;
 
   String _b;
   String get b => _b;
-  
+
   AppState({required String a, required String b})
       : this._a = a,
         this._b = b;
+
+  AppState from({String? a, String? b}) =>
+      AppState(a: a ?? this._a, b: b ?? this._b);
 }
+
+typedef Reduce();
+
+AppState addA(AppState current) => current.from(a: current.a + "a");
+AppState addB(AppState current) => current.from(b: current.b + "b");
+
+AppState removeA(AppState current) => current.from(
+    a: current.a.isEmpty ? "" : current.a.substring(0, current.a.length - 1));
+AppState removeB(AppState current) => current.from(
+    b: current.b.isEmpty ? "" : current.b.substring(0, current.b.length - 1));
