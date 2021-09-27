5cb8bacdabd44b4ebc62e91c5e3e7693e5d6ee6a
Robotoaster <swav.kulinski@gmail.com>
2021-09-27 15:25:11 +0100

    working

--- lib/form/form.provider.dart
+++ lib/form/form.provider.dart
@@ -12,19 +12,3 @@ final formStateProvider = Provider<FormState>((ref) {
 
 final userDataStateNotifierProvider =
     StateNotifierProvider<UserDataStateNotifier, UserData>((ref) => UserDataStateNotifier(ref));
-
-final firstNameControllerProvider =
-    Provider<TextEditingController>((ref) => TextEditingController());
-
-final lastNameControllerProvider =
-    Provider<TextEditingController>((ref) => TextEditingController());
-
-final emailControllerProvider = Provider<TextEditingController>((ref) => TextEditingController());
-
-final nonEmptyTextValidator = Provider<String? Function(String?)>((_) => (input) {
-      if (input?.isEmpty ?? true) {
-        return 'Field cannot be empty';
-      } else {
-        return null;
-      }
-    });
--- lib/form/text_form_field.dart
+++ lib/form/text_form_field.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class EmailForm extends StatelessWidget {
+final _nameRegexProvider = Provider<RegExp>((_) => RegExp(r"^[A-Z][a-z-']*$"));
+final _emailRegexProvider = Provider<RegExp>(
+    (_) => RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"));
+
+final _invalidEmailMessageProvider = Provider<String>((_) => 'This is not a valid email');
+final _invalidNameMessageProvider = Provider<String>((_) => 'Illegal characters used');
+
+class EmailForm extends ConsumerWidget {
   final Function(String) onChanged;
 
   EmailForm({Key? key, required this.onChanged}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => TextFormField(
+  Widget build(BuildContext context, ScopedReader watch) => TextFormField(
         autovalidateMode: AutovalidateMode.onUserInteraction,
         keyboardType: TextInputType.emailAddress,
-        validator: (input) =>
-            RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                    .hasMatch(input ?? '')
-                ? null
-                : 'This is not a valid email',
+        validator: (input) => watch(_emailRegexProvider).hasMatch(input ?? '')
+            ? null
+            : watch(_invalidEmailMessageProvider),
         onChanged: onChanged,
       );
 }
 
-class NameForm extends StatelessWidget {
-
+class NameForm extends ConsumerWidget {
   final Function(String) onChanged;
   NameForm({Key? key, required this.onChanged}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => TextFormField(
+  Widget build(BuildContext context, ScopedReader watch) => TextFormField(
         autovalidateMode: AutovalidateMode.onUserInteraction,
         keyboardType: TextInputType.name,
-        validator: (input) => RegExp(r"^[A-Z][a-z]*$").hasMatch(input ?? '')
+        validator: (input) => watch(_nameRegexProvider).hasMatch(input ?? '')
             ? null
-            : 'Illegal characters used',
-            onChanged: onChanged,
+            : watch(_invalidNameMessageProvider),
+        onChanged: onChanged,
       );
 }
